c43a93db7fb57bbe5e9a7db84ccfc54311fd7b41
an3wers <an3wer@yandex.ru>
2023-11-17 23:24:01 +0500

    added app header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,5 +1,56 @@
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./app-header.module.css";
+
+const NavLink = ({ children, path = "!#", extraClass = "", renderIcon }) => {
+  // TODO: Описать propTypes
+
+  const icon = renderIcon();
+  return (
+    <a
+      href={path}
+      className={`${styles.nav__link} ${extraClass} pl-5 pr-5 pb-4 pt-4 text text_type_main-default`}
+    >
+      {icon}
+      <span className="ml-2">{children}</span>
+    </a>
+  );
+};
+
 const AppHeader = () => {
-  return <></>;
+  return (
+    <header className={`${styles.header} pt-4 pb-4`}>
+      <nav className={styles.nav}>
+        <div
+          className={`${styles["nav__links-group"]} ${styles["nav__links-group_left"]}`}
+        >
+          <NavLink
+            extraClass={`mr-2 ${styles.nav__link_active}`}
+            renderIcon={() => <BurgerIcon type="primary" />}
+          >
+            Конструктор
+          </NavLink>
+          <NavLink renderIcon={() => <ListIcon type="secondary" />}>
+            Лента заказов
+          </NavLink>
+        </div>
+        <div className={styles.nav__logo}>
+          <Logo />
+        </div>
+        <div
+          className={`${styles["nav__links-group"]} ${styles["nav__links-group_right"]}`}
+        >
+          <NavLink renderIcon={() => <ProfileIcon type="secondary" />}>
+            Личный кабинет
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default AppHeader;
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,18 +1,20 @@
+import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerConstructor from "../burger/burger-constructor/burger-constructor";
 import BurgerIngredients from "../burger/burger-ingredients/burger-ingredients";
 import BurgerLayout from "../burger/burger-layout/burger-layout";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <AppHeader />
-      <BurgerLayout>
-        <BurgerConstructor />
-        <BurgerIngredients />
-      </BurgerLayout>
-    </div>
+      <div className={styles.container}>
+        <BurgerLayout>
+          <BurgerConstructor />
+          <BurgerIngredients />
+        </BurgerLayout>
+      </div>
+    </>
   );
 }
 

4debe43409603422d7641c4ebceaaf96dc919a7b
an3wers <an3wer@yandex.ru>
2023-11-25 16:36:36 +0500

    fix after review / round 1: added error handling for fetch response

--- src/api/ingredients.api.js
+++ src/api/ingredients.api.js
@@ -1,8 +1,8 @@
+import checkReponse from "../utlils/api/check-response";
+
 const baseApi = "https://norma.nomoreparties.space/api";
 
 export const getIngredients = async () => {
   const endPoint = "/ingredients";
-  const response = await fetch(baseApi + endPoint);
-  const data = await response.json();
-  return data;
+  return await fetch(baseApi + endPoint).then(checkReponse);
 };
